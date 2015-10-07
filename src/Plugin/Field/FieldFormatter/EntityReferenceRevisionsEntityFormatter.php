00022394d069907c683ed16b7afcb3e7a67dfaae
Vasile Chindris <vasile.chindris@amazee.com>
2015-10-07 15:48:45 +0200

    Some fixes to make this module work with the drupal 8 beta15.

--- src/Plugin/Field/FieldFormatter/EntityReferenceRevisionsEntityFormatter.php
+++ src/Plugin/Field/FieldFormatter/EntityReferenceRevisionsEntityFormatter.php
@@ -119,11 +119,11 @@ class EntityReferenceRevisionsEntityFormatter extends EntityReferenceRevisionsFo
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items, $langcode) {
+  public function viewElements(FieldItemListInterface $items) {
     $view_mode = $this->getSetting('view_mode');
     $elements = array();
 
-    foreach ($this->getEntitiesToView($items, $langcode) as $delta => $entity) {
+    foreach ($this->getEntitiesToView($items) as $delta => $entity) {
       // Protect ourselves from recursive rendering.
       static $depth = 0;
       $depth++;
